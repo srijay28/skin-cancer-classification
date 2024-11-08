f6d728c66a6ffbc0cda41fce59c670a798484146
Srijay Ketavarapu <ketavarapusrijay@gmail.com>
2024-11-08 10:44:02 +0530

    Added direct route to index view

--- SkinApp/urls.py
+++ SkinApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("index.html", views.index, name="index"),
+urlpatterns = [path("", views.index, name="index"),
                path("UserLogin.html", views.UserLogin, name="UserLogin"),	      
                path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
 	       path("Upload.html", views.Upload, name="Upload"),	      
